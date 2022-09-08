5b6ee51a492604a934302958c1ccb5f4c1b7d158
brend <brend@DESKTOP-36ES5SN>
2022-09-08 10:59:25 +0200

    Add project files.

---
+++ FindBob/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LabFindBob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Oppgave
+            //lag en metode som finner ut hvor mange bob's det er i stringen under.
+            string word = "bob";
+            string bob = "obobobobobobobbbooobboboobobobobobobooobbboobooboboboobboodfdfdob4boboboboob";
+            int count = 0;
+            foreach (Match match in Regex.Matches(bob, word, RegexOptions.IgnoreCase))
+            {
+                count++;
+            }
+            Console.WriteLine("{0}" + " Found " + "{1}" + " Times", word, count);
+        }
+    }
+}
